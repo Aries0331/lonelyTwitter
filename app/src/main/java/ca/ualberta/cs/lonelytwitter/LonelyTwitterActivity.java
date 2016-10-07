21373ddd02558542a74c09e9b974c714d0a0066e
Aries0331 <jinzhu@ualberta.ca>
2016-10-06 19:05:09 -0600

    add javadocs

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,18 +1,13 @@
+/*
+Copyright (C) 2016 Team 20, CMPUT301, University of Alberta - All Rights Reserved.
+You may use, copy or distribute this code under terms and conditions of University of Alberta
+and Code of Student Behavior.
+Please contact abc@abc.ca for more details or questions.
+*/
+
 package ca.ualberta.cs.lonelytwitter;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Date;
-
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -23,15 +18,61 @@ import android.widget.ListView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+
+/**
+ * This class is the main view class in lonelyTwitter class
+ * It deals with user inputs, saves/loads them in/from the file FILE_NAME (file.sav).
+ * <p >You can access the file from Android Device Monitor </p>
+ * <pre> pre-formatted    text </pre>
+ * <code>
+ *     pseudo-code that is used in this class as follows:
+ *     step1 <br>
+ *     step2 <br>
+ * </code>
+ * <ol>
+ *     <li>first item</li>
+ *     <li>second item</li>
+ *     <li>third item</li>
+ * <ol>
+ * @author Jinzhu
+ * @since 1.4
+ * @see NormalTweet
+ * @see java.io.BufferedReader
+ * @see TweetList
+ */
 public class LonelyTwitterActivity extends Activity {
 
+	/**
+	 * This is the name of the file that is saved in your virtual device.
+	 * You can access it through Android Device Monitor by selecting your app,
+	 * then data -> data -> file.sav
+	 * @see NormalTweet
+	 * @author A
+	 */
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
+	private ArrayList<Tweet> tweetList = new ArrayList<Tweet> ();
 	private ArrayAdapter<Tweet> adapter;
 
-	/** Called when the activity is first created. */
+/*
+Testing multi-time documentations
+Testing
+*/
+
+	/**
+	 * Called when the activity is first created.
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -56,13 +97,12 @@ public class LonelyTwitterActivity extends Activity {
 			}
 		});
 
-		clearButton.setOnClickListener(new View.OnClickListener(){
+		clearButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				tweetList.clear();
 				bodyText.setText("");
 				adapter.notifyDataSetChanged();
-				deleteInFile();
 
 			}
 		});
@@ -78,6 +118,26 @@ public class LonelyTwitterActivity extends Activity {
 		oldTweetsList.setAdapter(adapter);
 	}
 
+	/**
+	 *
+	 * @param inputString1 This is the prefix to the tweet
+	 * @param inputString2 This is the blah blah
+	 * @param inputString3
+	 * @param inputString4
+     * @return String
+     */
+	private String buildTweetString(String inputString1, String inputString2,
+								   String inputString3, String inputString4) {
+		String outputString = "The completed tweet string is: " + inputString1
+				+ "," + inputString2 + "," + inputString3 + "," + inputString4;
+		return outputString;
+	}
+
+	/**
+	 * This method loads the json file, generates the tweets from its contents.
+	 * @throws RuntimeException
+	 * @exception FileNotFoundException
+	 */
 	private void loadFromFile() {
 		ArrayList<String> tweets = new ArrayList<String>();
 		try {
@@ -85,7 +145,8 @@ public class LonelyTwitterActivity extends Activity {
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
 			Gson gson = new Gson();
 			//Code taken from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt September 22, 2016
-			Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
+			Type listType = new TypeToken<ArrayList<NormalTweet>>() {
+			}.getType();
 			tweetList = gson.fromJson(in, listType);
 
 		} catch (FileNotFoundException e) {
@@ -101,22 +162,6 @@ public class LonelyTwitterActivity extends Activity {
 	private void saveInFile() {
 		try {
 
-			FileOutputStream fos = openFileOutput(FILENAME,0);
-			OutputStreamWriter writer = new OutputStreamWriter(fos);
-			Gson gson = new Gson();
-			gson.toJson(tweetList, writer);
-			writer.flush();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			throw new RuntimeException();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			throw new RuntimeException();
-		}
-	}
-
-	private void deleteInFile() {
-		try {
 			FileOutputStream fos = openFileOutput(FILENAME, 0);
 			OutputStreamWriter writer = new OutputStreamWriter(fos);
 			Gson gson = new Gson();
@@ -130,4 +175,6 @@ public class LonelyTwitterActivity extends Activity {
 			throw new RuntimeException();
 		}
 	}
+
+
 }
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This is the package for classed of CMPUT301 lab project and blah blah ...
+ */
+package ca.ualberta.cs.lonelytwitter;
